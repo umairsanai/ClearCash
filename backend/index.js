58ff239c7d092cd3afa6a59eb27799c0fd54cbd8
Umair Anwar <anwarumair801@gmail.com>
2026-05-04 11:18:50 +0500

    Changed index.js to get ready for upload on render.

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,9 @@ dotenv.config({path: ".env"});
 import app from "./app.js";
 import { initialize, gracefulShutdown } from "./helpers.js";
 
-const server = app.listen(process.env.SERVER_PORT, process.env.SERVER_HOST, () => {
-   console.log("Server started...."); 
-});
+const server = app.listen(process.env.PORT || 3000, process.env.SERVER_HOST || "0.0.0.0");
 
 initialize();
-
 
 process.on('SIGTERM', gracefulShutdown(server));
 process.on('SIGINT', gracefulShutdown(server));
